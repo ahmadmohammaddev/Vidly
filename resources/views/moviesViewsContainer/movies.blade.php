c6572a0cd1b19a8bed91c2b8b8c9a633e248a8b4
ahmadmohammaddev <57289588+ahmadmohammaddev@users.noreply.github.com>
2020-08-24 21:17:28 +0300

    creating master blade template file

--- resources/views/moviesViewsContainer/movies.blade.php
+++ resources/views/moviesViewsContainer/movies.blade.php
@@ -1,41 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <header class="jumbotron">
-        <div class="container">
-            <div class="col-md-10">
-                <h1>The Movies!</h1>
-                <p>Evrey movie is a new adventure</p>
-            </div>
-            <div class="col-md-2">
-                {{-- Date : {{ $date }} <br/> Time : {{ $time }}  --}}
-            </div>
-            
-        </div>
-    </header>
+@extends('master')
+
+@section('content')
+    
 
 
-    <div class="row">
-        @foreach($genres as $key => $value)
-        <div class="col-md-3">
-            <div class="thumbnail">
-                <img src="images/{{$value}}">
-                <h1><a class="btn btn-primary">{{$key}}</a></h1>
-            </div>
-        </div> 
-        @endforeach
-        
-    </div>
 
-    <footer class="container">
-        &copy; All Right Reserved for Ahmed Muhammed - 2020
-    </footer>
-        
-</body>
-</html>
+@endsection
