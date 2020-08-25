1ce7d40ad72d65dfef4a8100cd35405687a6bba9
ahmadmohammaddev <57289588+ahmadmohammaddev@users.noreply.github.com>
2020-08-25 18:15:47 +0300

    using bootstrap classes to make creating forms btr

--- resources/views/moviesViewsContainer/admin.blade.php
+++ resources/views/moviesViewsContainer/admin.blade.php
@@ -7,33 +7,38 @@
 <div class="container">
 
     <div class="panel panel-default">
-        <div class="panel-heading">
-            Managing Genres
+        <div class="panel-heading text-center">
+            <h1>Managing Genres</h1> 
         </div>
 
         <div class="panel-body">
-            <h2><br/> Creating new Genre</h2> <hr/>
-
-            {{-- Creating New Genre --}}
-            {{-- <form action="genre" method="POST" enctype="multipart/form-data">
-                {!! csrf_field() !!}
-                Enter the name of the Genre:<input type="text" name="genre_name" /><br />
-                Upload an image: <input type="file" name="image" />
-                <br />
-                <button class="btn btn—default" type="submit">
-                    <img src="{{asset('/images/add.ico')}}" width="25px" height="25px" />Create
-                </button>
-            </form> --}}
+            <div class="text-center">
+                <h2 ><br/> Creating new Genre</h2>
+            </div>
+            <hr/>
 
             {!! Form::open(['url' => 'genre', 'files' => true]) !!}
-                Enter the name of the Genre:{!! Form::text('genre_name'); !!}
-                Upload an image:{!! Form::file('image'); !!}
-                {!! Form::submit('Create',["class"=>"btn btn-info"]);!!}
+            <div class="input-group mb-1">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="">Name of Genre</span>
+                </div>
+                {!!   Form::text('genre_name', '',['class' => 'form-control']) !!}
+            </div>
+            <div class="input-group">
+                <div class="custom-file">                  
+                  {!! Form::file('image',['class' => 'custom-file-input', 'type' => 'file', 'id' => 'inputGroupFile04']); !!}
+                  <label class="custom-file-label" for="inputGroupFile04">Upload an image</label>
+                </div>
+                <div class="input-group-append">
+                  {!! Form::submit('Create',["class"=>"btn btn-info"]);!!}
+                </div>
+            </div>
             {!! Form::close() !!}
 
 
         </div>{{-- end of body --}}
-        
+        <hr/>
+        <br/>
         @if($genres != null)
             <table c1ass="table">
                 <tr>
@@ -46,22 +51,6 @@
                 @foreach($genres as $genre)
                 <!-- TabLe -->
                 <tr>
-                    {{-- Updating Existing Genre --}}
-                    {{-- <form action="genre/{{$genre->id}}" method="POST">
-                        {!! csrf_field() !!}
-                        {{ method_field('PUT') }}
-                        <td>
-                            <input type="text" name="genre_name" value="{{ $genre->name}}">
-                        </td>
-                        <td>
-                            <span class="label label-default">{{ $genre->movies_total }}</span>
-                        </td>
-                        
-                        <td>
-                            <button name="action" class="btn btn—default" type="submit"><img src="{{asset('/images/update.png')}}" width="25px" height="25px" alt="blue"></button>
-                        </td>
-                    </form> --}}
-
                     {!! Form::open(['url' => 'genre/'.$genre->id, 'method' => 'put']) !!}
                         <td>
                             {!! Form::text('genre_name', $genre->name); !!}
@@ -74,20 +63,11 @@
                         </td>
                     {!! Form::close() !!}
 
-                    <td>
-                    {{-- Deleting Genre --}}
-                    {{-- <form action="genre/{{$genre->id}}" method="POST">
-                        {!! csrf_field() !!}
-                        {{ method_field('DELETE') }}
-                        <button name="action" class="btn btn—default" type="submit"><img src="{{asset('/images/delete.png')}}" width="25px" height="25px" alt="blue"></button>
-                    </form> --}}
-
-                    {!! Form::open(['url' => 'genre/'.$genre->id, 'method' => 'delete']) !!}
-                        {!! Form::submit('Delete',["class"=>"btn btn-danger"]);!!}                        
-                    {!! Form::close() !!}
-
-
-                    </td>
+                        <td>
+                            {!! Form::open(['url' => 'genre/'.$genre->id, 'method' => 'delete']) !!}
+                                {!! Form::submit('Delete',["class"=>"btn btn-danger"]);!!}                        
+                            {!! Form::close() !!}
+                        </td>
                 </tr>
         
                 @endforeach
