a8576e5958be1d2a8b271864f7501d0ce61d46ad
ahmadmohammaddev <57289588+ahmadmohammaddev@users.noreply.github.com>
2020-08-28 22:25:44 +0300

    Adding links to the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,15 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="/genre">Avialable Genres</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/admin">Managing Genres</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/summary">Avialable Movies</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -65,7 +74,7 @@
                                         @csrf
                                     </form>
                                 </div>
-                            </li>
+                            </li>                            
                         @endguest
                     </ul>
                 </div>
