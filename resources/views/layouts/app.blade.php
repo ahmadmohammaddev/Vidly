00989a6d41f65f61c9e44cb2f17daa28e6625531
ahmadmohammaddev <57289588+ahmadmohammaddev@users.noreply.github.com>
2020-08-31 22:28:47 +0300

    correcting the bug of update form not working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,9 +78,30 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+
+    <main role="main">      
+
+        @if(Session::has('m'))
+        <div class="container">
+          <?php $a = []; $a = session()->pull('m'); ?>
+          <div role="alert" class="alert alert-{{ $a[0] }}">
+            {{ $a[1] }}
+          </div>
+        </div>
+        @endif
+  
+        @yield('content')
+  
+        
+      </main>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="../../assets/js/vendor/holder.min.js"></script>
 </body>
 </html>
