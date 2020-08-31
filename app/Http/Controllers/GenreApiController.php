bee617df566b7db01d75b59d62ebc58fc0c4997f
ahmadmohammaddev <57289588+ahmadmohammaddev@users.noreply.github.com>
2020-09-01 02:47:28 +0300

    Dealing with 404

--- app/Http/Controllers/GenreApiController.php
+++ app/Http/Controllers/GenreApiController.php
@@ -18,7 +18,7 @@ class GenreApiController extends Controller
 
     public function show($id)
     {
-        return response()->json(Genre::find($id), 200);
+        return response()->json(Genre::findOrFail($id), 200);
     }
 
     public function store(Request $request)
